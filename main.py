378013888168522b8b0e4bd04b3287b3ea3ba2a5
Daniil Grishenok <grishenokdaniil5@gmail.com>
2024-12-17 21:49:37 +0300

    added wsgi.py

--- main.py
+++ main.py
@@ -185,4 +185,4 @@ def export_schedule():
     return send_file(csv_output, mimetype='text/csv', as_attachment=True, download_name='schedule.csv')
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=10000)
+    app.run(debug=True)
